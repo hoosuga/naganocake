2b517276a555d40670708016726047620a432479
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-05-19 10:58:02 +0000

    [Add] 管理者用ビュー

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
 
   # 顧客用
   devise_for :customers,skip: [:passwords], controllers: {
