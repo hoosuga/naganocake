b3dd7137bb33e2633332b7d49bb2ff26deb7e93f
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-05-20 05:07:07 +0000

    [Add] 管理者用ヘッダー

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,6 +2,11 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    admin_home_path
+  end
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +23,7 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  #protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
