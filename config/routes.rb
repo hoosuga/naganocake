07060fa856fcf8b6a1532d1f855a4145268dca8a
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-05-20 07:34:07 +0000

    [Add] public/ordersコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   namespace :admin do
     get '/' => 'homes#top', as:'home'
   end
@@ -8,10 +9,13 @@ Rails.application.routes.draw do
     get '/about' => 'homes#about', as:'about'
     get '/items' => 'items#index', as:'item'
     get '/items/:id' => 'items#show', as:'item_show'
-    get '/costomers' => 'costomers#show', as:'costomers_show'
-    get '/customers/information/edit' => 'costomers#edit', as:'costomers_edit'
-    get '/customers/confirm_withdraw' => 'costomers#confirm_withdraw', as:'costomers_confirm_withdraw'
+    get '/costomers' => 'costomers#show', as:'costomer_show'
+    get '/customers/information/edit' => 'costomers#edit', as:'costomer_edit'
+    get '/customers/confirm_withdraw' => 'costomers#confirm_withdraw', as:'costomer_confirm_withdraw'
     get '/cart_items' => 'cart_items#index', as:'cart_items'
+    get '/orders/new' => 'orders#new', as:'order_new'
+    get '/orders' => 'orders#index', as:'order'
+    get '/orders/:id' => 'orders#show', as:'order_show'
   end
 
   # 管理者用
