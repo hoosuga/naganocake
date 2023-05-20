0bcb0b3ff6b749fdc4728be2870729b960e15466
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-05-20 07:21:11 +0000

    [Add] public/costomersコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/' => 'homes#top', as:'home'
   end
-  
-  namespace :public do
+
+  scope module: :public do
     root to: 'homes#top'
     get '/about' => 'homes#about', as:'about'
-    get '/items' => 'items#index', as:'items'
-    get '/items/:id' => 'items#show', as:'items_show'
+    get '/items' => 'items#index', as:'item'
+    get '/items/:id' => 'items#show', as:'item_show'
+    get '/costomers' => 'costomers#show', as:'costomers_show'
+    get '/customers/information/edit' => 'costomers#edit', as:'costomers_edit'
+    get '/customers/confirm_withdraw' => 'costomers#confirm_withdraw', as:'costomers_confirm_withdraw'
   end
- 
+
   # 管理者用
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
