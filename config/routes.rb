6e8e29e877ed6e560f87d5f4be66ecb62a46daad
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-05-20 06:33:51 +0000

    [Add] public/homesとpublic/itemsコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
     get '/' => 'homes#top', as:'home'
   end
   
+  namespace :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about', as:'about'
+    get '/items' => 'items#index', as:'items'
+    get '/items/:id' => 'items#show', as:'items_show'
+  end
+ 
   # 管理者用
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
@@ -11,9 +18,8 @@ Rails.application.routes.draw do
   # 顧客用
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
-  sessions: 'public/sessions'
+  sessions: 'public/sessions',
   }
 
-  root to: 'homes#top'
-  get '/about' => 'homes#about', as:'about'
+
 end
