65f6bb1826508fb17b7ad68a968be0c87187f9fa
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-05-28 11:05:56 +0000

    [Add] 同一商品が存在する場合の処理

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,18 +5,27 @@ class Public::CartItemsController < ApplicationController
  end
 
  def create
-   @cart_item = CartItem.new(cart_item_params)
-   @cart_item.save
-   redirect_to cart_items_path
+   @cart_items = current_customer.cart_items
+
+   if @cart_items.find_by(item_id: params[:cart_item][:item_id])
+    new_amount = cart_item.amount + @cart_item.amount
+    @cart_item.update_attribute(:amount, new_amount)
+   else
+     @cart_item = CartItem.new(cart_item_params)
+     @cart_item.save
+   end
+     redirect_to cart_items_path
  end
 
  def update
+  @cart_items = current_customer.cart_items
   @cart_item = CartItem.find(params[:id])
   @cart_item.update(cart_item_params)
   redirect_to cart_items_path
  end
 
  def destroy
+  @cart_items = current_customer.cart_items
   @cart_item = CartItem.find(params[:id])
   @cart_item.destroy
   redirect_to cart_items_path
