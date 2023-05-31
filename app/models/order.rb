4e0a7cac72097fa2bf518cd8adb304c9531e4390
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-05-31 10:18:02 +0000

    [Add] enum導入

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,8 @@ class Order < ApplicationRecord
     item.tax_price * amount
   end
 
+  enum payment_method: { credit_card: 0, transfer: 1 }
+
+  enum order_status: { waiting_for_payment: 0, payment_confirmation: 1, making: 2, preparing_to_ship: 3, shipped: 4 }
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,7 @@
 class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
+
+  enum crafting_status: { not_make: 0, waiting_to_make: 1, making: 2, completed: 3 }
+
 end
