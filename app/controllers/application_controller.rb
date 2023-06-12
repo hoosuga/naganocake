1363417615e8f3c1754e0dd5a6fefabed23c7fe8
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-06-12 10:10:19 +0000

    [Update] レイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    customer_show_path(resource)
+  end
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+      root_path
+    elsif resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
 end
